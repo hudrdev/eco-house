26fd9ce33d820cf9cc2b16d2a826c4b12805cd01
Vitalii Hudanets <hud.rdev@gmail.com>
2019-05-30 12:42:37 +0200

    Updates

---
+++ precache-manifest.8494e0a8895beedd43b8fabc4169dcb4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc02c98c5eb478c881fa2bf34bdef75",
+    "url": "/eco-house/index.html"
+  },
+  {
+    "revision": "6d2c4065cd291d7b4a51",
+    "url": "/eco-house/static/css/2.35be9652.chunk.css"
+  },
+  {
+    "revision": "72b5bfa30044bda85775",
+    "url": "/eco-house/static/css/main.65f22cd1.chunk.css"
+  },
+  {
+    "revision": "6d2c4065cd291d7b4a51",
+    "url": "/eco-house/static/js/2.8f292d7f.chunk.js"
+  },
+  {
+    "revision": "72b5bfa30044bda85775",
+    "url": "/eco-house/static/js/main.d8beaab4.chunk.js"
+  },
+  {
+    "revision": "163770724ef9fb7d76a0",
+    "url": "/eco-house/static/js/runtime~main.6f00f2e5.js"
+  },
+  {
+    "revision": "3309bc970fb9a0c54595de5973a365e9",
+    "url": "/eco-house/static/media/GothamPro-Black.3309bc97.eot"
+  },
+  {
+    "revision": "a3d7d652af07c3610c453b4a51c788c7",
+    "url": "/eco-house/static/media/GothamPro-Bold.a3d7d652.woff"
+  },
+  {
+    "revision": "c15ee62b232cedc240947b6d814fb750",
+    "url": "/eco-house/static/media/GothamPro-Bold.c15ee62b.ttf"
+  },
+  {
+    "revision": "ee9b8c537d217fef51d4bca8cf436d7d",
+    "url": "/eco-house/static/media/GothamPro-Bold.ee9b8c53.eot"
+  },
+  {
+    "revision": "6fe466da15644ccc370d3b2ce3a19f6f",
+    "url": "/eco-house/static/media/GothamPro-Medium.6fe466da.eot"
+  },
+  {
+    "revision": "b86e995a10856a8b8e222fe8ff00a74f",
+    "url": "/eco-house/static/media/GothamPro-Medium.b86e995a.woff"
+  },
+  {
+    "revision": "c161369173f822acd66d2ff8eea64c52",
+    "url": "/eco-house/static/media/GothamPro-Medium.c1613691.ttf"
+  },
+  {
+    "revision": "3100f91bbd9e9ca9ecd00255cac7d11c",
+    "url": "/eco-house/static/media/GothamPro.3100f91b.ttf"
+  },
+  {
+    "revision": "3cb46b372ab77d032576b9f70c83d1ff",
+    "url": "/eco-house/static/media/GothamPro.3cb46b37.woff"
+  },
+  {
+    "revision": "c31a8e097cb6c2901857d8c686f0063d",
+    "url": "/eco-house/static/media/GothamPro.c31a8e09.eot"
+  },
+  {
+    "revision": "aa0d629e49331e40fe2ff22fbea9e3fe",
+    "url": "/eco-house/static/media/section1-bg.aa0d629e.jpg"
+  },
+  {
+    "revision": "3fbb59bb6765afcff7e22b438c305be4",
+    "url": "/eco-house/static/media/section2-bg1.3fbb59bb.jpg"
+  },
+  {
+    "revision": "564d0995e3bdc618e591a90f8fabab60",
+    "url": "/eco-house/static/media/section2-bg2.564d0995.jpg"
+  },
+  {
+    "revision": "8b86c282503cf7e002b67258e486e6b2",
+    "url": "/eco-house/static/media/section2-p1-1.8b86c282.jpg"
+  },
+  {
+    "revision": "b9a4b5518d4d1a9adff2bf28787eec7a",
+    "url": "/eco-house/static/media/section2-p2-1.b9a4b551.jpg"
+  },
+  {
+    "revision": "de67ebfe2ebcc8fb9fdceec6bab30df5",
+    "url": "/eco-house/static/media/section4-bg.de67ebfe.png"
+  },
+  {
+    "revision": "9576c9c8f4e8c5ec8785b62e6aeed49c",
+    "url": "/eco-house/static/media/section4-p1-1.9576c9c8.jpg"
+  },
+  {
+    "revision": "bf14265c14df6c64d8e3287810587f65",
+    "url": "/eco-house/static/media/section4-p2-1.bf14265c.jpg"
+  },
+  {
+    "revision": "69e1a1dd83ba7eefad2985d872809499",
+    "url": "/eco-house/static/media/section6-map.69e1a1dd.jpg"
+  },
+  {
+    "revision": "64ed17926852f5ea27eb65a44a2797ab",
+    "url": "/eco-house/static/media/woods.64ed1792.jpg"
+  }
+]);
